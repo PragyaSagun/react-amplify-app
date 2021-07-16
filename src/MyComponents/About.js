61ba5f3ef89a43c7194db75de9f3548d0a6bb1be
Pragya Thakur <TPRAGYA@amazon.com>
2021-07-16 15:20:52 +0530

    upload file added

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,9 +2,38 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
+        <div className="border">
+            <div>
+                <label htmlFor="Audit Stream" style="font-size: 150%">Choose Audit Stream</label>
+                <select name="AUDIT" id="audit" style="font-size: 120%">
+                    <option value="ALL">ALL</option>
+                    <option value="PGP Covid Audits">PGP Covid Audits</option>
+                    <option value="WCP">WCP</option>
+                    <option value="audi">MVA</option>
+                    <option value="WCP">WCP</option>
+                    <option value="Heatmap">Heatmap</option>
+                    <option value="Event Based Audits">Event Based Audits</option>
+                    <option value="Escalation Related Audits">Escalation Related Audits</option>
+
+                </select>
+                <br><br>
+                    <form className="form" id="myForm">
+                        <input type="file" style="color:darkblue;font-size: 120%"/>
+                    </form>
+                    <br><br>
+            </div>
+            <div className="buttonclass">
+                <button className="button" id="uploadbutton">Upload File</button>
+            </div>
+
+            <p style="color: black;font-size: 150%;font-weight: bold;padding-left: 20px;padding-top: 50px">Instructions
+                for Upload:</p>
+            <ul className="b" style="font-size: 180%">
+                <li>Format:</li>
+                <li></li>
+                <li></li>
+            </ul>
+
         </div>
     )
 }
